d1ecb050d1ee39f89ae0d9c331505a0ad7b05e60
untitlez <tleza_championship2@hotmail.com>
2025-08-24 05:25:22 +0700

    delete : openAi

--- routes/openAi.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { openAi } = require("../controllers/openAi");
-
-const router = express.Router();
-
-router.post("/openAi", openAi);
-
-module.exports = router;
